3350f227d44bd105a468ffc33b23931d2a56daf2
TheRedBoxIllusion <masoninoz+github@gmail.com>
2025-09-06 22:31:47 -0400

    Finished the collision system and fixed some bugs

--- PhysicsTesting/Game1.cs
+++ PhysicsTesting/Game1.cs
@@ -16,6 +16,7 @@ namespace PhysicsTesting
         Texture2D blackBlock;
         Texture2D whiteBlock;
         Texture2D playerSprite;
+        Texture2D collisionSprite;
 
         
         PhysicsEngine physicsEngine;
@@ -28,6 +29,8 @@ namespace PhysicsTesting
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
+            this.TargetElapsedTime = TimeSpan.FromSeconds(1d/120d);
 
             worldContext = new WorldContext();
             worldContext.initialiseWorld();
@@ -50,11 +53,13 @@ namespace PhysicsTesting
             blackBlock = new Texture2D(_graphics.GraphicsDevice, 1, 1);
             whiteBlock = new Texture2D(_graphics.GraphicsDevice, 1, 1);
             playerSprite = new Texture2D(_graphics.GraphicsDevice, 1, 1);
+            collisionSprite = new Texture2D(_graphics.GraphicsDevice, 1, 1);
 
 
             blackBlock.SetData<Color>(new Color[]{ Color.Black });
             whiteBlock.SetData<Color>(new Color[] { Color.White });
             playerSprite.SetData<Color>(new Color[] { Color.Red });
+            collisionSprite.SetData<Color>(new Color[] { Color.Green });
 
             base.Initialize();
         }
@@ -82,8 +87,14 @@ namespace PhysicsTesting
             }
             if (Keyboard.GetState().IsKeyDown(Keys.W)) {
                 if (player.isOnGround) {
-                    player.accelerationY += 150;
+                    player.accelerationY += 250;
                 }
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.R)) {
+                player.x = 10;
+                player.y = 10;
+                player.velocityX = 0;
+                player.velocityY = 0;
             }
             
 
@@ -125,8 +136,35 @@ namespace PhysicsTesting
                 }
             }
 
-            //System.Diagnostics.Debug.WriteLine(player.x + ", " + player.y);
+            
             _spriteBatch.Draw(playerSprite, new Rectangle((int)player.x, (int)player.y, (int)(0.9 * worldContext.pixelsPerBlock), 2 * worldContext.pixelsPerBlock), Color.White);
+            
+            
+             /* A segment of code that draws an outline around all the blocks considered for the players collision
+            int entityLocationInGridX = (int)Math.Floor(player.x / worldContext.pixelsPerBlock);
+            int entityLocationInGridY = (int)Math.Floor(player.y / worldContext.pixelsPerBlock);
+            int entityGridWidth = (int)Math.Ceiling((double)player.collider.Width / worldContext.pixelsPerBlock);
+            int entityGridHeight = (int)Math.Ceiling((double)player.collider.Height / worldContext.pixelsPerBlock);
+            int p = worldContext.pixelsPerBlock;
+            for (int x = entityLocationInGridX - 1; x < entityLocationInGridX + entityGridWidth + 1; x++)
+            { //A range of x values on either side of the outer bounds of the entity
+                for (int y = entityLocationInGridY - 1; y < entityLocationInGridY + entityGridHeight + 1; y++)
+                {
+                    Rectangle entityCollider = new Rectangle((int)player.x, (int)player.y, player.collider.Width, player.collider.Height);
+
+                    Rectangle blockRect = new Rectangle(x * p, y * p, p, p);
+                    if (blockRect.Intersects(entityCollider) && worldContext.worldArray[x,y] != 0)
+                    {
+                        
+                        _spriteBatch.Draw(collisionSprite, new Rectangle(x * p, y * p, p, p), Color.White);
+                    }
+                        _spriteBatch.Draw(playerSprite, new Rectangle(x * p, y * p, p, 2), Color.White);
+                    _spriteBatch.Draw(playerSprite, new Rectangle(x * p, y * p, 2, p), Color.White);
+                    _spriteBatch.Draw(playerSprite, new Rectangle(x * p, (y + 1) * p, p, 2), Color.White);
+                    _spriteBatch.Draw(playerSprite, new Rectangle((x + 1) * p, y * p, 2, p), Color.White);
+                }
+            }
+            // */
 
             _spriteBatch.End();
             base.Draw(gameTime);
@@ -161,14 +199,29 @@ namespace PhysicsTesting
             {
                 worldArray[i, 21] = 1;
             }
+            for (int i = 24; i < 32; i++) {
+                worldArray[i, 17] = 1;
+            }
+            //for (int i = 28; i < 32; i++)
+            //{
+               // worldArray[i, 18] = 1;
+            //}
             //initialise the array with a z shape of 1s, to represent flat ground then a drop to another flat piece
         }
     }
 
     public class PhysicsEngine
     {
+        //Current bug: When the player collides with a block horizontally, there's a singular frame where they aren't colliding
+        //anymore, allowing this annoying rubberbanding visual. Using a basic visual of the collision, it is considered to be
+        //colliding with the horizontal block. So it's something else weird.
+        bool helpDebug = false;
         public double blockSizeInMeters { get; } = 0.6; //The pixel size in meters can be found by taking this value and dividing it by pixelsPerBlock
         WorldContext wc;
+
+        int horizontalOverlapMin = 5;
+        int verticalOverlapMin = 5;
+
 
         public PhysicsEngine(WorldContext worldContext)
         {
@@ -202,6 +255,9 @@ namespace PhysicsTesting
             entity.velocityX += (entity.accelerationX) * timeElapsed;
             entity.velocityY += (entity.accelerationY) * timeElapsed;
 
+            System.Diagnostics.Debug.WriteLine(entity.accelerationY * timeElapsed);
+            
+            if (helpDebug) { System.Diagnostics.Debug.WriteLine(entity.velocityX); }
             //Sets the velocity to 0 if it is below a threshold. Reduces excessive sliding and causes the drag function to actually reach a halt
             if ((entity.velocityX > 0 && entity.velocityX < entity.minVelocityX) || (entity.velocityX < 0 && entity.velocityX > -entity.minVelocityX))
             {
@@ -222,11 +278,13 @@ namespace PhysicsTesting
         public void applyVelocityToPosition(PhysicsObject entity, double timeElapsed) {
             //Adds the velocity * time passed to the x and y variables of the entity. Y is -velocity as the y-axis is flipped from in real life (Up is negative in screen space)
             //Converts the velocity into pixel space. This allows for realistic m/s calculations in the actual physics function and then converted to pixel space for the location
+            System.Diagnostics.Debug.WriteLine(entity.velocityY + ", " + entity.accelerationY);
             entity.updateLocation(entity.velocityX * timeElapsed * (wc.pixelsPerBlock/blockSizeInMeters), -entity.velocityY * timeElapsed * (wc.pixelsPerBlock / blockSizeInMeters));
         }
 
 
         public void detectBlockCollisions(PhysicsObject entity) {
+            helpDebug = false;
             //Gets the blocks within a single block radius around the entity. Detects if they are colliding, then if they are, calls another method
             int entityLocationInGridX = (int)Math.Floor(entity.x / wc.pixelsPerBlock);
             int entityLocationInGridY = (int)Math.Floor(entity.y / wc.pixelsPerBlock);
@@ -245,20 +303,163 @@ namespace PhysicsTesting
                             Rectangle blockRect = new Rectangle(x * wc.pixelsPerBlock, y * wc.pixelsPerBlock, wc.pixelsPerBlock, wc.pixelsPerBlock);
                             if (blockRect.Intersects(entityCollider))
                             {
-                                if (entity.accelerationY < 0)
+                                (double x, double y) collisionNormal = computeCollisionNormal(entityCollider, blockRect);
+
+
+                                //If the signs are unequal on either the velocity or the acceleration then the forces should cancel as the resulting motion would be counteracted by the block
+                                if (((Math.Sign(collisionNormal.y) != Math.Sign(entity.velocityY) && entity.velocityY != 0) || (Math.Sign(collisionNormal.y) != Math.Sign(entity.accelerationY) && entity.accelerationY != 0)) && collisionNormal.y != 0)
                                 {
-                                    entity.velocityY = 0;
-                                    entity.accelerationY = 0;
-                                    entity.y = blockRect.Y - entityCollider.Height + 1; //Plus one prevents the block from re-falling
+                                    entity.velocityY -= (1 + entity.bounceCoefficient) * entity.velocityY;
+                                    entity.accelerationY -= entity.accelerationY;
+
+                                    if (Math.Sign(collisionNormal.y) > 0) {
+                                        entity.isOnGround = true;
+                                    }
+
+                                    if (Math.Sign(collisionNormal.y) > 0)
+                                    {
+                                        entity.y = blockRect.Y - entityCollider.Height + 1;
+                                    }
+                                    else {
+                                        entity.y = blockRect.Bottom - 1;
+                                    }
                                 }
-                                entity.isOnGround = true;
-                                System.Diagnostics.Debug.WriteLine("Has collided with a block! " + entity.accelerationY + ", ");
+                                
+                                if(((Math.Sign(collisionNormal.x) != Math.Sign(entity.velocityX) && entity.velocityX != 0) || (Math.Sign(collisionNormal.x) != Math.Sign(entity.accelerationX ) && entity.accelerationX != 0)) && collisionNormal.x != 0)
+                                {
+                                    helpDebug = true;
+                                    System.Diagnostics.Debug.WriteLine(entity.x + ", " + entity.velocityX + ", " + entity.accelerationX);
+
+                                    entity.velocityX -= (1 + entity.bounceCoefficient) * entity.velocityX;
+                                    entity.accelerationX -= entity.accelerationX;
+                                    System.Diagnostics.Debug.WriteLine(entity.x + ", " + entity.velocityX + ", " + entity.accelerationX);
+                                    if (Math.Sign(collisionNormal.x) > 0)
+                                    {
+                                        entity.x = blockRect.Right -  1;
+                                    }
+                                    else
+                                    {
+                                        entity.x = blockRect.Left - entityCollider.Width + 1;
+                                    }
+                                    System.Diagnostics.Debug.WriteLine(entity.x);
+                                }
+                                
                             }
                         }
                     }
                 }
             }
 
+        }
+
+        public (double, double) computeCollisionNormal(Rectangle entityCollider, Rectangle blockRect) {
+            (double x, double y) collisionNormal = (0, 0);
+            (int x, int y) approximateCollisionDirection = (entityCollider.Center.X - blockRect.Center.X, entityCollider.Center.Y - blockRect.Center.Y);
+            
+            if (approximateCollisionDirection.x <= 0 && approximateCollisionDirection.y <= 0)
+            { //Bottom Right from the player
+                int verticalOverlap = entityCollider.Bottom - blockRect.Top;
+                int horizontalOverlap = entityCollider.Right - blockRect.Left;
+                if (horizontalOverlap < horizontalOverlapMin)
+                {
+                    horizontalOverlap = 0;
+                }
+                if (verticalOverlap < verticalOverlapMin)
+                {
+                    verticalOverlap = 0;
+                }
+                if (verticalOverlap != 0 || horizontalOverlap != 0)
+                {
+                   
+                    if (verticalOverlap > horizontalOverlap)
+                    {
+                        
+                        return (-1, 0);
+                    }
+                    else
+                    {
+                        return (0, 1);
+                    }
+                }
+            } else if (approximateCollisionDirection.x >= 0 && approximateCollisionDirection.y <= 0)
+            { //Bottom Left from the player
+                int verticalOverlap = entityCollider.Bottom - blockRect.Top;
+                int horizontalOverlap = blockRect.Right - entityCollider.Left;
+                if (horizontalOverlap < horizontalOverlapMin)
+                {
+                    horizontalOverlap = 0;
+                }
+                if (verticalOverlap < verticalOverlapMin)
+                {
+                    verticalOverlap = 0;
+                }
+                if (verticalOverlap != 0 || horizontalOverlap != 0)
+                {
+                   
+                    if (verticalOverlap > horizontalOverlap)
+                    {
+                        
+                        return (1, 0);
+                    }
+                    else
+                    {
+                        return (0, 1);
+                    }
+                }
+            } else if (approximateCollisionDirection.x <= 0 && approximateCollisionDirection.y >= 0)
+            { //Top Right from the player
+                int verticalOverlap = blockRect.Bottom - entityCollider.Top;
+                int horizontalOverlap = entityCollider.Right - blockRect.Left;
+                if (horizontalOverlap < horizontalOverlapMin)
+                {
+                    horizontalOverlap = 0;
+                }
+                if (verticalOverlap < verticalOverlapMin)
+                {
+                    verticalOverlap = 0;
+                }
+                if (verticalOverlap != 0 || horizontalOverlap != 0)
+                {
+                    
+
+                    if (verticalOverlap > horizontalOverlap)
+                    {
+                        return (-1, 0);
+                    }
+                    else
+                    {
+                        return (0, -1);
+                    }
+                }
+            }
+            else if (approximateCollisionDirection.x >= 0 && approximateCollisionDirection.y >= 0)
+            { //Top Left from the player
+                int verticalOverlap = blockRect.Bottom - entityCollider.Top;
+                int horizontalOverlap = blockRect.Right - entityCollider.Left;
+                if (horizontalOverlap < horizontalOverlapMin)
+                {
+                    horizontalOverlap = 0;
+                }
+                if (verticalOverlap < verticalOverlapMin)
+                {
+                    verticalOverlap = 0;
+                }
+                if (verticalOverlap != 0 || horizontalOverlap != 0)
+                {
+                    
+                    if (verticalOverlap > horizontalOverlap)
+                    {
+                        
+                        return (1, 0);
+                    }
+                    else
+                    {
+                        return (0, -1);
+                    }
+                }
+            }
+            System.Diagnostics.Debug.WriteLine("Has returned a (0,0) collision normal from " + approximateCollisionDirection.x + ", " + approximateCollisionDirection.y);
+            return collisionNormal;
         }
 
     }
@@ -299,7 +500,7 @@ namespace PhysicsTesting
             kY = 0.0;
             bounceCoefficient = 0.0;
             minVelocityX = 0.5;
-            minVelocityY = 0.1;
+            minVelocityY = 0.01;
             isOnGround = false;
 
             collider = new Rectangle(0, 0, wc.pixelsPerBlock, wc.pixelsPerBlock);
@@ -313,7 +514,7 @@ namespace PhysicsTesting
         }
 
         public void onBlockCollision(int blockX, int blockY) {
-        
+            
         }
     }
 
@@ -325,8 +526,13 @@ namespace PhysicsTesting
             y = 10.0;
             //It's weird because k is unitless, however the fact that I'm in the world of pixels/second means that realistic drag coefficients don't work very well. 
             //I think it's because v^2 has a massive change with the pixel to block ratio. My math's is probably just wrong, so I'll merely account for it by using unrealistic numbers
-            kX = 3;
+            kX = 0;
             kY = 0.01;
+
+
+            velocityX = 4.1;
+            
+            bounceCoefficient = 0.75;
 
             collider = new Rectangle(0, 0, (int)(0.9 * wc.pixelsPerBlock), 2 * wc.pixelsPerBlock);
         }
